941f8e10170ffe5f0194f784da3de534cc1229fb
Didier Lane <didier.lane@mail.com>
2026-04-10 09:15:43 +0200

    implemented check for newer alpine tag
    refactored github check release

--- make/check.mk
+++ make/check.mk
@@ -1,4 +1,5 @@
 .PHONY: check
 check: jq # 🔄 Checks for newer versions of dependencies
 	$(call github_check_release_version,$(JQ_REPOSITORY),$(JQ_VERSION),$(JQ_ASSET))
+	$(call docker_check_version,alpine,$(ALPINE_VERSION))
 	$(call github_check_release_version,$(SQUID_REPOSITORY),SQUID_$(SQUID_VERSION),$(SQUID_ARCHIVE) $(SQUID_SIGNATURE))
--- make/functions.mk
+++ make/functions.mk
@@ -30,21 +30,3 @@ $(call checksum,$(4),$(5))
 $(call extract,$(5))
 $(call executable,$(5))
 endef
-
-define github_check_release_version
-cache="$$( mktemp )"
-result="$$( curl -sSL https://api.github.com/repos/$(1)/releases/latest > "$${cache}" )"
-latest="$$( jq -r '.tag_name' < "$${cache}" )"
-if [[ "$${latest}" == "$(2)" ]]; then
-	$(call message,✅,Repository $(hl)$(1)$(rs) is up to date with latest version $(hl)$${latest}$(rs))
-else
-	$(call message,💡,Repository $(hl)$(1)$(rs) new version is $(hl)$${latest}$(rs))
-	if [ ! -z "$(3)" ]; then
-		for asset in $(3); do
-			digest="$$( jq -r '.assets[] | select(.name == "'"$${asset}"'") | .digest' < "$${cache}" )"
-			$(call message,📦,Asset $(hl)$${asset}$(rs) digest is $(hl)$${digest}$(rs))
-		done
-	fi
-fi
-rm "$${cache}"
-endef
