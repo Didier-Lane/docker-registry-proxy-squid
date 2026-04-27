3bc356a1ce93c462f5c27d337bba3046c843a31f
Didier Lane <didier.lane@mail.com>
2026-04-27 09:27:06 +0200

    refactor: using integrated jq/check target

--- make/check.mk
+++ make/check.mk
@@ -1,5 +1,4 @@
 .PHONY: check
-check: jq # 🔄 Checks for newer versions of dependencies
-	$(call github_check_release_version,$(JQ_REPOSITORY),$(JQ_VERSION),$(JQ_ASSET))
+check: jq jq/check # 🔄 Checks for newer versions of dependencies
 	$(call dockerhub_check_version,debian,$(DEBIAN_VERSION),13-slim,trixie)
 	$(call github_check_release_version,$(SQUID_REPOSITORY),SQUID_$(SQUID_VERSION),$(SQUID_ARCHIVE) $(SQUID_SIGNATURE))
